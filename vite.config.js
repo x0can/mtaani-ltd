38bc57807895cce3734cd01ef992c293982a81ed
x0can <alexmwaura43@gmail.com>
2026-02-17 21:31:00 +0300

    project files v2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/mtaani-ltd/",
+export default defineConfig(() => {
+  // Vercel sets process.env.VERCEL = "1"
+  const isVercel = process.env.VERCEL === "1";
+
+  return {
+    plugins: [react()],
+    base: isVercel ? "/" : "/mtaani-ltd/", // GH Pages needs repo path
+  };
 });
